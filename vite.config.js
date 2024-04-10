f0d9a2bc18e2b3c7aed2799d04e3f1bb9c6baf03
Роман Шамов <deadsher@mail.ru>
2024-04-11 03:08:14 +0500

    Настраивает окружение

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import reactRefresh from '@vitejs/plugin-react-refresh'
+import path from 'path';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), reactRefresh()],
+  resolve: {
+    alias: {
+      '@': path.resolve('./src'),
+      '@assets': path.resolve('./src/assets'),
+      '@components': path.resolve('./src/components'),
+      '@utils': path.resolve('./src/utils'),
+    },
+  },
 })
